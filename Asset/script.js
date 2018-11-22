b4d87e9a42098a620964f6d972ae628cd98484c2
Hafidzil Khairi <hafidzilkhairi00@gmail.com>
2018-11-22 18:41:17 +0700

    Finished materi text's page | 1NF page

--- Asset/script.js
+++ Asset/script.js
@@ -1,3 +1,10 @@
+function memerahkan(e){
+    if(e.val()!='' || e.val()){
+        e.css({'border-color':'#ebebeb'})
+    }else{
+        e.css(e.css({ 'border-color': 'red' }))
+    }
+}
 $(()=>{
     $('#ref1').mouseenter(()=>{
         $('#ref1 div').animate({'left':'-683px'},'fast');
@@ -38,5 +45,66 @@ $(()=>{
         $('.setelahMateri').css({
             'color': 'rgba(100, 100, 100,.8)'
         })
+    })
+    $('.jendelaDiskusi').css({
+        'left': -$(window).width()
+    })
+    $('.layarGelapDiskusi').css({
+        'top': $(window).height()
+    })
+    $(window).resize(()=>{
+        $('.jendelaDiskusi').css({
+            'left': -$(window).width()
+        })
+        $('.layarGelapDiskusi').css({
+            'top': $(window).height()
+        })
+    });
+    $('.tombolClose').click(()=>{
+        $('.jendelaDiskusi').animate({
+            "left": -$(window).width()
+        },{
+            duration:400,
+            complete:()=>{
+                $('.layarGelapDiskusi').animate({
+                    'top': $(window).height()
+                },400)
+            }
+        })
+    })
+    $('#tombolDiskusi').click(()=>{
+        console.log('saya')
+        $('.layarGelapDiskusi').animate({
+            "top": 0
+        }, {
+            duration: 400,
+            complete: () => {
+                $('.jendelaDiskusi').animate({
+                    'left': 0
+                }, 400)
+            }
+        })
+    })
+    $('.layarGelapDiskusi').click(() => {
+        $('.jendelaDiskusi').animate({
+            "left": -$(window).width()
+        }, {
+                duration: 400,
+                complete: () => {
+                    $('.layarGelapDiskusi').animate({
+                        'top': $(window).height()
+                    }, 400)
+                }
+            })
+    })
+    $('.namaUser').keydown(()=>{
+        memerahkan($('.namaUser'));
+    }).keyup(()=>{
+        memerahkan($('.namaUser'));
+    })
+    $('.isiPesanUser').keydown(() => {
+        memerahkan($('.isiPesanUser'));
+    }).keyup(() => {
+        memerahkan($('.isiPesanUser'));
     })
 })
